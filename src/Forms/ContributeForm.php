3dd82b64464463b9bace9588d3e7ea2666fe8132
Dylan Rushby-Riley <dylanrushbyriley@gmail.com>
2017-12-20 10:22:15 +0000

    First Commit

---
+++ src/Forms/ContributeForm.php
@@ -0,0 +1,75 @@
+<?php
+/**
+* @file
+* Contains \Drupal\dylans_form\Form\ContributeForm.
+*/
+
+namespace Drupal\dylans_form\Form;
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Component\Utility\UrlHelper;
+
+/**
+* Contribute form.
+*/
+class ContributeForm extends FormBase {
+ /**
+  * {@inheritdoc}
+  */
+ public function getFormId() {
+    return 'dylans_form_contribute_form';
+ }
+
+ /**
+  * {@inheritdoc}
+  */
+ public function buildForm(array $form, FormStateInterface $form_state) {
+    $form['title'] = array(
+      '#type' => 'textfield',
+      '#title' => t('Your Name'),
+      '#required' => TRUE,
+    );
+    $form['video'] = array(
+      '#type' => 'textfield',
+      '#title' => t('Your Favorite YouTube Video'),
+    );
+    $form['video'] = array(
+      '#type' => 'textfield',
+      '#title' => t('Youtube video'),
+    );
+    $form['develop'] = array(
+      '#type' => 'checkbox',
+      '#title' => t('I am a CheckBox, Check me!'),
+    );
+    $form['description'] = array(
+      '#type' => 'textarea',
+      '#title' => t('What is your Favorite Video about?'),
+    );
+    $form['submit'] = array(
+      '#type' => 'submit',
+      '#value' => t('Submit'),
+    );
+    return $form;
+ }
+
+ /**
+  * {@inheritdoc}
+  */
+ public function validateForm(array &$form, FormStateInterface $form_state) {
+   // Validate video URL.
+    if (!UrlHelper::isValid($form_state->getValue('video'), TRUE)) {
+      $form_state->setErrorByName('video', $this->t("The video url '%url' is invalid.", array('%url' => $form_state->getValue('video'))));
+    }
+ }
+
+ /**
+  * {@inheritdoc}
+  */
+ public function submitForm(array &$form, FormStateInterface $form_state) {
+   // Display result.
+    foreach ($form_state->getValues() as $key => $value) {
+      drupal_set_message($key . ': ' . $value);
+    }
+  }
+}
+?>
